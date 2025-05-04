946bd9a70c380cc9d35588076234ec96fde17422
Vinodhkumar <vinodhkumar0810@gmail.com>
2025-05-04 23:55:38 +0530

    commit

--- src/EX_01_Java_Basics/HelloWorld.java
+++ src/EX_01_Java_Basics/HelloWorld.java
@@ -1,4 +1,7 @@
 package EX_01_Java_Basics;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
--- src/EX_01_Java_Basics/Lab_001_HelloWorld.java
+++ src/EX_01_Java_Basics/Lab_001_HelloWorld.java
@@ -1,4 +1,8 @@
 package EX_01_Java_Basics;
 
 public class Lab_001_HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
